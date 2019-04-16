0ea4d988502dd87f299890bdc68a88d244512bca
Alex gama <alexgamaes@gmail.com>
2019-04-16 08:16:11 +0000

    Add modify controller

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -31,24 +31,76 @@ exports.create_database = function(req, res) {
       }
 };
 
+
 exports.view_database = function(req, res) {
+  var couchbase = require('couchbase')
+
+  var cluster = new couchbase.Cluster('couchbase://localhost/');
+  cluster.authenticate('sync_gateway', 'tesina');
+  var bucket = cluster.openBucket('staging');
+
+  var N1qlQuery = couchbase.N1qlQuery;
+
+  query = N1qlQuery.fromString('SELECT * from staging WHERE v IS NOT MISSING');
+  bucket.query(query, function(err, rows, meta) {
+	console.log(err);
+        var s = "Data(" + rows.length + "): " + JSON.stringify(rows);
+        var n = 0;
+
+        for(row in rows) {
+          console.log(rows[row].staging.v);
+          n = (n + rows[row].staging.v) % 1000000007;
+        }
+
+        s = '<p>Checksum: ' + n + '</p><p>' + s + '</p>';
+
+	       res.send(s);
+    });
+
+    //res.send('NOT IMPLEMENTED: View database');
+};
+
+exports.modify_database = function(req, res) {
 var couchbase = require('couchbase')
 
     var cluster = new couchbase.Cluster('couchbase://localhost/');
     cluster.authenticate('sync_gateway', 'tesina');
     var bucket = cluster.openBucket('staging');
+    bucket.operationTimeout=500*1000;
 
-    var N1qlQuery = couchbase.N1qlQuery;
+    var docs_number = req.params.docs_number;
 
-    query = N1qlQuery.fromString('SELECT * from staging');
-    bucket.query(query, function(err, rows, meta) {
-	console.log(err);
-        var s = "LOL: (" + rows.length + ")" + JSON.stringify(rows);
+    /*var N1qlQuery = couchbase.N1qlQuery;
 
-	res.send(s);
-    });
+    query = N1qlQuery.fromString('SELECT * from staging');/
+    bucket.query(query, function(err, rows, meta) {*/
+	  //console.log(err);
+      var percentage = req.params.percentage;
+      if(percentage == 0) {
+        return res.send("CHANGED: " + percentage + "%");
+      }
+      var step = 100.0 / percentage;
 
-    //res.send('NOT IMPLEMENTED: View database');
+      if(step == 0) {
+        return res.send("INVALID STEP: " + percentage + "%");
+      }
+
+      for(let i = 0.0; Math.round(i) < docs_number; i += step) {
+        bucket.replace("id" + Math.round(i), {
+          'v': getRandomInt(10000000) + 1
+        }, {},
+        function (err, result) {
+          console.log("change in: " + "id" + Math.round(i));
+          console.log(err);
+
+          if(Math.round(i + step) >= docs_number) {
+            return res.send("FINISHED!");
+          }
+        });
+      }
+
+  //});
+  //res.send('NOT IMPLEMENTED: View database');
 };
 
 exports.delete_database = function(req, res) {
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var index_controller = require('../controllers/indexController');
 
 /* GET home page. */
 router.get('/create_database/:docs_number', index_controller.create_database);
+router.get('/modify_database/:docs_number/:percentage', index_controller.modify_database);
 router.get('/view_database', index_controller.view_database);
 router.get('/delete_database', index_controller.delete_database);
 
